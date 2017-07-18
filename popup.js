51289f65f110b49861ca2f41f6c8d241379cd22d
belaltaher8 <belal.taher@duke.edu>
2017-07-18 14:30:02 -0400

    finally got it working

--- popup.js
+++ popup.js
@@ -1,14 +1,5 @@
-<script>
-    var count = 1;
-    function setColor(btn) {
-        var property = document.getElementById(btn);
-        if (count == 0) {
-            property.style.backgroundColor = "#FFFFFF"
-            count = 1;
-        }
-        else {
-            property.style.backgroundColor = "#000000"
-            count = 0;
-        }
-    }
-</script>
+function myFunction() {
+    document.getElementById("result").innerHTML = 'Hello Belal!';
+}
+
+document.getElementById("button").onclick = myFunction;
