7f991a69d1685f14318a7032c0cd79b4df8cc90b
belaltaher8 <belal.taher@duke.edu>
2017-07-22 16:01:09 -0400

    uploaded picture and finished skeleton for front end
    uploaded picture and finished skeleton for front end

--- popup.js
+++ popup.js
@@ -1,5 +1,11 @@
-function myFunction() {
-    document.getElementById("result").innerHTML = 'Hello Belal!';
+function inputFocus(){
+    i = document.getElementById("myText");
+    if(i.value==i.defaultValue){ i.value=""; i.style.color="#000"; }
+}
+function inputBlur(){
+    i = document.getElementById("myText");
+    if(i.value==""){ i.value=i.defaultValue; i.style.color="#888"; }
 }
 
-document.getElementById("button").onclick = myFunction;
+document.getElementById("myText").onfocus = inputFocus;
+document.getElementById("myText").onblur = inputBlur;
