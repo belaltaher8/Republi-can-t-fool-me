a39f820cf9447260923fc8fdaaa2dd0f859a1e82
belaltaher8 <belal.taher@duke.edu>
2017-12-29 02:13:33 -0500

    finished front end

--- Extension/popup.js
+++ Extension/popup.js
@@ -7,6 +7,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	submitButton.addEventListener('click', function(){
 
+		document.getElementById("loader").style.visibility = "visible";
+		document.getElementById("result").style.visibility = "visible";
 
 		var url = document.getElementById('urlBox').value;
 
@@ -15,35 +17,42 @@ document.addEventListener('DOMContentLoaded', function() {
 		xhr.open("POST", "https://murmuring-basin-19452.herokuapp.com/classify", true);
 		xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8"); 
 
+		document.getElementById
+
 		xhr.onload = function (e) { 
 			if (xhr.readyState === 4) { 
 				if (xhr.status === 200) { 
 				var respJSON = JSON.parse(xhr.responseText); 
+				document.getElementById("loader").style.visibility = "hidden";
 					console.log(respJSON["Answer"]);
 					if(respJSON["Answer"] < 0.4){
-						document.getElementById("result").innerHTML = "Republican";
-						document.body.style.background = "rgba(255,0,0,0.7)";
+						document.getElementById("result").innerHTML = "The article has a Republican bias";
+						document.body.style.background = "rgba(255,0,0,0.6)";
 					}
 					else if(respJSON["Answer"] > 0.6 && respJSON["Answer"] < 1){
-						document.getElementById("result").innerHTML = "Democrat";
-						document.body.style.backgroundColor = "rgba(0,0,255,0.7)";
+						document.getElementById("result").innerHTML = "The article has a Democrat bias";
+						document.body.style.backgroundColor = "rgba(0,0,255,0.6)";
 
 					}
 					else if(respJSON["Answer"] <= 0.6 && respJSON["Answer"] >= 0.4){
-						document.getElementById("result").innerHTML = "Neutral";
-						document.body.style.backgroundColor = "rgba(0,0,0,0.7)";
+						document.getElementById("result").innerHTML = "It doesn't seem like this article has any bias";
+						document.body.style.backgroundColor = "rgba(0,0,0,0.6)";
 					}
 					else{
 						document.getElementById("result").innerHTML = "Sorry. Something's wrong with the article!";
 					}
 				}
 				else { 
+					document.getElementById("loader").style.visibility = "hidden";
+					document.getElementById("result").style.visibility = "hidden";
 					document.getElementById("result").innerHTML = "Sorry. Something's wrong with the server!";
 				} 
 			} 
 		}; 
 
 		xhr.onerror = function (e) { 
+			document.getElementById("loader").style.visibility = "hidden";
+			document.getElementById("result").style.visibility = "hidden";
 			console.error(xhr.statusText); 
 		}; 
 
